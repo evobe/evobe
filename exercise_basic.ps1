16dbf037851ef8d6ed2ebb93065eda4955694d7d
Abra <ab.techstuff@gmail.com>
2024-06-02 12:29:49 -0400

    adding workouts

--- exercise_basic.ps1
+++ exercise_basic.ps1
@@ -1,10 +1,32 @@
+#getting list of last exercise offered
+$donelist = ".\done_list.csv"
+$tpast = get-content $donelist | Select-Object -Last 1
+write-host = "Welcome! The last workout offered was - $tpast - did you complete?"
+$workoutanswer = read-host "Did you complete the workout? y/n"
+if($workoutanswer.ToUpper() -eq "Y"){
+    write-host "Great job! The workout has been marked as completed. Let's get at it!"
+    $newlast = $tpast + ",done"
+    (Get-Content $donelist) + $newlast | Set-Content -Path $donelist
+} elseif ($workoutanswer.ToUpper() -eq "N") {
+    Write-Host "That's OK - do you want to repeat the workout ?"
+    $repeat = read-host "y/n"
+    if($repeat.ToUpper() -eq "Y"){
+        write-host "Today's workout is $tpast - enjoy!"
+        $todai = $tpast
+    }elseif($repeat.ToUpper() -eq "N"){
+        write-host "OK - new workout being served"
+    } else {write-host "you did not enter y or n and I have no way of dealing with that right now so start over"}
+}else{
+    write-host "did not enter y or n and I have no way of dealing with that right now so start over"
+}
 #setting up an exercise selection menu
-$workout = import-csv .\Elist.txt -Delimiter ','
-write-host "Welcome! Are you ready for today's workout? Or do you want to review past workouts"
+$workout = import-csv .\Elist.txt -Delimiter ',' 
+write-host "Let's get it, ready for a new workout? Or do you want to review past workouts"
 $answer = read-host "Select (1) - new workout `nSelect (2) - review last 5 workouts"
 
 
 switch ($answer) {
+    #random number for switch that selects the workouts
     {$_ -eq 1} {$rando = Get-Random -max 5 -Min 1; write-host $rando}
     {$_ -eq 2} {write-host "2 has been selected"}
     default {
@@ -12,24 +34,74 @@ switch ($answer) {
     }
 }
 
-$selector = Get-Random -min 1 -max 10
+#selector for the workout in the column
+$selector = Get-Random -min 1 -max 3
 
+#adding gym day since it's a lot
+$gym1 = @"
+   flat db press 1x4-6,1x8-10
+   db rom dead 2x8-10
+   2 grip lat pulldown
+    1x10-12 med overhand grip
+    1x10-12 underhand close grip
+    <then maybe some overhead curls with same grip, lower weight>
+   db step up 1x8-10 reps per leg
+   overhead cable tricep ext 1x12-15
+    <cable just behind head, lock elbows>
+   lateral raise 1x12-15
+   leg press toe press 1x12-15 (straight leg calf raise)
+"@
+
+$gym2 = @"
+    hack squat or bb squat 1x4-6, 1x8-10
+   a. high incline smith press 2x10-12 like 45 or 60 degree angle
+   b. t-bar row or incline rows 2x10-12 wide and middle grip
+   seated leg curl 1x10-12 - failure, dropset - failure again, lean       over machine
+   ez bar bicep curl 1x12-15, 
+    then 4 reps (rest 3 secs) 4 reps, continue until fail
+   cable crunch 1x12-15
+    then double dropset
+"@
+
+#checking what random number was selected
 switch ($rando){
     {$_ -eq 1} {write-host "you're running";
-    $newrando = get-random -min 1 -max 3;
-    write-host "TODAY'S WORKOUT:" $workout."1"[$newrando]
+    $todai = $workout."1"[$selector];
+    $value = "run"
+    write-host "TODAY'S WORKOUT:" $todai
     }
     {$_ -eq 2} {write-host "you're working out";
-    $newrando = get-random -min 1 -max 3;
-    write-host "TODAY'S WORKOUT:" $workout."2"[$newrando]}
-    {$_ -eq 3} {write-host "you're swimming";
-    $newrando = get-random -min 1 -max 3;
-    write-host "TODAY'S WORKOUT:" $workout."3"[$newrando]}
-    {$_ -eq 4} {write-host "you're biking";
-    $newrando = get-random -min 1 -max 3;
-    write-host "TODAY'S WORKOUT:" $workout."4"[$newrando]}
-    {$_ -eq 5} {write-host "you're doing yoga"
-    $newrando = get-random -min 1 -max 3;
-    write-host "TODAY'S WORKOUT:" $workout."5"[$newrando]}
-}
+    $value = "gymming"
+    $todai = $workout."2"[$selector];
+    #adding in the actual workout
+    if($todai = 1){$todai = $gym1}
+    elseif($todai = 2){$todai = $gym2}
+    else{$todai = $todai}
+    write-host "TODAY'S WORKOUT:" $todai
+    }
+   {$_ -eq 3} {write-host "you're swimming";  
+   $value =  "swim"
+   $todai = $workout."3"[$selector];
+    write-host "TODAY'S WORKOUT:" $todai 
+    }
+    {$_ -eq 4} {write-host "you're biking"; 
+    $value = "bike"
+    $todai = $workout."4"[$selector];
+    write-host "TODAY'S WORKOUT:" $todai
+    }
 
+    {$_ -eq 5} {write-host "you're doing yoga"
+    $value = "yoga"
+    $todai = $workout."5"[$selector];
+    write-host "TODAY'S WORKOUT:" $todai
+    }
+    }
+
+$daydate = get-date -Format "ddd MM/dd/yyyy"
+
+add-content -Path $donelist.trimend() -Value "`n$daydate, $value, $todai"
+
+<## it works! - some scenarios - skip workout if it already says done.
+check date on last workout, if last workout is more than 5 days ask to repeat
+figure out why there's a space
+##>
